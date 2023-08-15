097b147b439f79270cacf25ab36837b9d10e41b6
MARIA VITORIA POLINI <06551199143@ifms.edu.br>
2023-08-15 09:06:26 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './componentes/header/Header'
+import Footer from './componentes/footer/Footer'
+import './App.css'
+
+function App() {
+  return (
+      <>
+      <Header/>
+      <Footer/>
+      </>
+  )
+}
+
+export default App
