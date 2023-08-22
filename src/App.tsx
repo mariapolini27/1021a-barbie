28acccefb4633a7d91e155a99d001969102cd411
mariapolini27 <maria.polini@estudante.ifms.edu.br>
2023-08-22 10:18:18 -0400

    atividade 22/08

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Header from './componentes/header/Header'
-import Footer from './componentes/footer/Footer'
+import Filme from './componentes/filme/Filme'
+import Footer  from './componentes/footer/Footer'
 import './App.css'
 
 function App() {
   return (
-      <>
+    <div>
       <Header/>
+      <Filme/>
       <Footer/>
-      </>
+    </div>
+    
   )
 }
-
 export default App
