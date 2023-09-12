c51fcaffddc2d779b5b1069133c774f9a76a54cb
mariapolini27 <maria.polini@estudante.ifms.edu.br>
2023-09-12 10:12:45 -0400

    barbie

--- src/componentes/Main/Main.tsx
+++ src/componentes/Main/Main.tsx
@@ -1,17 +1,51 @@
+import{ useState} from 'react'
 import Filme from './../Filme/Filme'
 import './Main.css'
+type FilmeType = {
+    id:number,
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
 export default function Main(){
-  let textodigitado = 'Barbie'
-    function TrataTexto(){
-        console.log("Digitado")
-        textodigitado = "TERE"
+
+  //let textodigitado = 'Barbie'
+  const [texto,setTexto]=useState("")
+  const filmes = [
+    {
+        id:1,
+        titulo:'Barbie',
+        sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+        imagem:'/barbie.png'
+    },
+    { 
+        id:2,
+        titulo:  'Ken',
+        sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+        imagem:'/Ken.png'
+    },
+    {
+        id:3,
+        titulo:  'Ken e Barbie',
+        sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+        imagem:'KeneBarbie.webp'
+
+    }
+  ]
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+       
+       // console.log(e.target.value)
+       setTexto(e.target.value)
+        
     }
     return(
              <>
             <div className="campo_pesquisa">
                 <p> Busque um filme</p>
                 <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme' onChange={TrataTexto} />
-                <p>Digitado: {textodigitado} </p>
+                {!texto && <p>Resultados Para:{texto} </p> }
+
+{}             
             </div>
 
         <main className="content-main">
