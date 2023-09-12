22306f56e287b80da9516e944804c68bff253a7b
mariapolini27 <maria.polini@estudante.ifms.edu.br>
2023-09-12 09:07:06 -0400

    barbie

--- src/componentes/Main/Main.tsx
+++ src/componentes/Main/Main.tsx
@@ -1,7 +1,19 @@
 import Filme from './../Filme/Filme'
 import './Main.css'
 export default function Main(){
+  let textodigitado = 'Barbie'
+    function TrataTexto(){
+        console.log("Digitado")
+        textodigitado = "TERE"
+    }
     return(
+             <>
+            <div className="campo_pesquisa">
+                <p> Busque um filme</p>
+                <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme' onChange={TrataTexto} />
+                <p>Digitado: {textodigitado} </p>
+            </div>
+
         <main className="content-main">
             <Filme titulo= 'Barbie'
             sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
@@ -16,5 +28,6 @@ export default function Main(){
             imagem='KeneBarbie.webp'
             />
         </main>
+        </>
     )
 }
