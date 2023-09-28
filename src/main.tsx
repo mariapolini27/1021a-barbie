f469c6bc45695ca6a84c6f9bb4ee8076e602d1fc
mariapolini27 <maria.polini@estudante.ifms.edu.br>
2023-09-28 08:31:12 -0400

    projeto barbie

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import './main.css'
+import Header from './componentes/header/Header'
+import Main from './componentes/Main/Main'
 
+import{createBrowserRouter, RouterProvider} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <>
+     <Header/>
+    <Main/>
+    </>,
+    
+  
+  },
+  {
+    path: "/noticias",
+    element: <div>Pagina de Noticias!</div>,
+  },
+  {
+    path: "/sobre",
+    element: <div>Pagina sobre!</div>,
+  },
+  {
+    path: "/contato",
+    element: <div>Pagina de Contato!</div>,
+  },
+
+
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
